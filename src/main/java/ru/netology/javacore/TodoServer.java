5711bb9f0848e7b7679bf3c00533f4620db8dcd4
Sergei Gridnev <sergeigr32@gmail.com>
2023-07-19 23:47:59 +0300

    Initial commit

--- src/main/java/ru/netology/javacore/TodoServer.java
+++ src/main/java/ru/netology/javacore/TodoServer.java
@@ -8,14 +8,31 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class TodoServer {
-    //...
+    protected int port;
+
+    protected Todos todos;
 
     public TodoServer(int port, Todos todos) {
-        //...
+        this.port = port;
+        this.todos = todos;
     }
 
     public void start() throws IOException {
         System.out.println("Starting server at " + port + "...");
-        //...
+        try (ServerSocket serverSocket = new ServerSocket(8989);) {
+            while (true) {
+                try (
+                        Socket socket = serverSocket.accept();
+                        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                        PrintWriter out = new PrintWriter(socket.getOutputStream());
+                ) {
+                    // обработка одного подключения
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Не могу стартовать сервер");
+            e.printStackTrace();
+        }
+
     }
 }
--- src/main/java/ru/netology/javacore/Todos.java
+++ src/main/java/ru/netology/javacore/Todos.java
@@ -1,21 +1,37 @@
 package ru.netology.javacore;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class Todos {
-    //...
+
+    public String[] tasks = new String[7];
 
     public void addTask(String task) {
-        //...
+        for (int i = 0; i < tasks.length; i++) {
+            if (tasks[i] == null) {
+                tasks[i] = task;
+                break;
+            } else if (tasks[i].equals(task)) {
+                break;
+            }
+        }
     }
 
     public void removeTask(String task) {
-        //...
+        for (int i = 0; i < tasks.length; i++) {
+            if (tasks[i].equals(task)) {
+                tasks[i] = null;
+                break;
+            }
+        }
     }
 
     public String getAllTasks() {
-        //...
+        return Arrays.stream(tasks)
+                .filter(Objects::nonNull)
+                .collect(Collectors.joining("\n"));
     }
 
 }
